78a6c05b07c222365a61796fc3eebc54adf7f7e2
Gerry Whitworth <gerrywhitworth@sierrasystems.com>
2018-10-10 08:41:13 -0700

    changed script a bit

--- PCSTOTS/PCSTOTS.sql
+++ PCSTOTS/PCSTOTS.sql
@@ -1,6 +1,10 @@
 DECLARE @p_RY INT;
 SET @p_RY = 2017;
-SELECT [AG].[Area Code] AS [AREA], 
+DECLARE @p_CN [INT];
+SET @p_CN = 10;
+
+SELECT
+       [AG].[Area Code] AS [AREA], 
        [AG].[Jurisdiction Code] AS [JUR], 
        [FA].[Property Class Code] AS [ASSD PC], 
        SUM([FA].[Net School Land Value]) AS [SCHOOL_LAND], 
@@ -9,7 +13,8 @@ FROM [edw].[FactAllAssessedAmounts] AS [FA]
      INNER JOIN [edw].[dimAssessmentGeography] AS [AG] ON [FA].[dimAssessmentGeography_SK] = [AG].[dimAssessmentGeography_SK]
                                                           AND AG.[Roll Category Code] = '1'
 WHERE [FA].[Roll Year] = @p_RY
-      AND [FA].[Property Class Code] <> 'UNK'
+AND [FA].[Cycle Number] <= @p_CN
+      AND [FA].[Property Class Code] NOT IN('UNK', '99')
 GROUP BY [AG].[Area Code], 
          [AG].[Jurisdiction Code], 
          [FA].[Property Class Code]
