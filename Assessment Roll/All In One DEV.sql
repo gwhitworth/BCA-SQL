5e7101d660b4b8348b483278601548ca974a9141
Gerry Whitworth <gerrywhitworth@sierrasystems.com>
2018-12-05 16:47:25 -0800

    After I added comma delimited column to pivot query

--- Assessment Roll/All In One DEV.sql
+++ Assessment Roll/All In One DEV.sql
@@ -65,7 +65,24 @@ WITH FMT([dimFolio_SK],
      (
          SELECT [dimFolio_SK], 
                 [CAT], 
-                [CODE]
+                (
+             SELECT DISTINCT 
+                    [smcList] = STUFF(
+             (
+                 SELECT ','+[TC].[BCA Code]
+                 FROM [EDW].[edw].[bridgeFolioMinorTax] AS [id2]
+                      INNER JOIN [edw].[dimMinorTaxCode] AS [TC]
+                      ON [id2].[dimMinorTaxCode_SK] = [TC].[dimMinorTaxCode_SK]
+                 WHERE [id1].[dimFolio_SK] = [id2].[dimFolio_SK]
+                       AND [id1].[Minor Tax Category Code] = [id2].[Minor Tax Category Code]
+                 GROUP BY [TC].[BCA Code] FOR XML PATH(''), TYPE
+             ).value('.', 'varchar(max)'), 1, 1, '')
+             FROM [EDW].[edw].[bridgeFolioMinorTax] AS [id1]
+                  INNER JOIN [edw].[dimMinorTaxCode] AS [TC]
+                  ON [id1].[dimMinorTaxCode_SK] = [TC].[dimMinorTaxCode_SK]
+             WHERE [dimFolio_SK] = [FMT].[dimFolio_SK]
+                   AND [id1].[Minor Tax Category] = [FMT].[CAT]
+         ) AS [CODE]
          FROM [FMT]
      ) [C] PIVOT(MAX([CODE]) FOR [CAT] IN([LA - LOCAL AREA], 
                                           [SM - SPECIFIED MUNICIPAL], 
